31824351555e65216c33fa132dbd9a5e92239ca2
Priyanka Bammanahalli <priyankasb1803@gmail.com>
2024-09-24 22:40:43 +0530

    portfolio

---
+++ script.js
@@ -0,0 +1,18 @@
+//let x = document.getElementsByClassName("main")[0];
+
+// document.querySelector(".cross").style.display = "none";
+// document.querySelector(".hamburger").addEventListener("click", () => {
+//   document.querySelector(".sidebar").classList.toggle("sidebarGo");
+
+//   if (document.querySelector(".sidebar").classList.contains("sidebarGo")) {
+//     document.querySelector(".ham").style.display = "inline";
+//     document.querySelector(".cross").style.display = "none";
+//     //x.classList.remove("test");
+//   } else {
+//     document.querySelector(".ham").style.display = "none";
+//     //x.classList.add("test");
+//     setTimeout(() => {
+//       document.querySelector(".cross").style.display = "inline";
+//     }, 350);
+//   }
+// });
